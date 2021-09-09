fad99231066abb0a862eda0faf75f1432e2a5e8f
Bhargav <bhargavkadali39@gmail.com>
2021-09-09 21:55:37 +0530

    Create password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,23 @@
+# pip install cowsay
+
+import cowsay
+import string as s
+from random import *
+
+n=int(input("Enter length of password\n"))
+
+if(n<8):
+    print('Min of 8 characters are must for secure password\ndefault value 8 is assigned')
+    
+n+=8
+
+cond = str(input('Do u want to include special symbols in your passcode. \n type "y/n"\n').lower())
+
+ch = s.ascii_letters + s.digits
+
+if cond == 'y':
+    ch = ch + s.punctuation
+    
+passw = "".join(choice(ch)for x in range(8,n))
+
+cowsay.tux(passw)
